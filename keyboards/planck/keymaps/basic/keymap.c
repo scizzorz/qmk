c4a5b9943aecf533f9ec5b9ef449a9632ba2f288
John Weachock <jweachock@gmail.com>
2017-09-04 15:54:47 -0400

    Add scizzorz-planck keymap

--- keyboards/planck/keymaps/basic/keymap.c
+++ keyboards/planck/keymaps/basic/keymap.c
@@ -8,63 +8,54 @@
 // Fillers to make layering more clear
 #define _______ KC_TRNS
 #define XXXXXXX KC_NO
+#define _ KC_TRNS
+#define X KC_NO
+
+#define BASE 0
+#define PURP 1
+#define ORNG 2
+#define BLUE 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-/* Qwerty
+/*
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * | Esc  |   q  |   w  |   e  |   r  |   t  |   y  |   u  |   i  |   o  |   p  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Esc  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Tab  |   a  |   s  |   d  |   f  |   g  |   h  |   j  |   k  |   l  |   ;  | Entr |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
+ * | Shft |      |   z  |   x  |   c  |   v  |   b  |   n  |   m  |   ,  |   .  | Shft |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+ * | Ctrl | Alt  |  A   |  B   |  C   |    Space    |  C   |  B   |  A   | Alt  | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
-[0] = {
-  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
-  {KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT },
-  {KC_RSFT, KC_LCTL, KC_LALT, KC_LGUI, M(1),    KC_SPC,  KC_SPC,   M(2),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT}
+[BASE] = {
+  {KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC},
+  {GUI_T(KC_TAB),KC_A,KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT },
+  {KC_LSFT, KC_CAPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  RSFT_T(KC_SLSH)},
+  {KC_LCTL, M(BLUE), KC_LALT, M(ORNG), M(PURP), KC_SPC,  KC_SPC,  M(ORNG), M(PURP), KC_LGUI, KC_RCTL, M(BLUE)}
 },
 
-/* Lower
- * ,-----------------------------------------------------------------------------------.
- * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  |   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |      |      |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Reset |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-[1] = {
-  {KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,S(KC_NUHS),S(KC_NUBS),_______, _______, _______},
-  {RESET,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+[PURP] = {
+  {KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR, KC_PERC,  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQT },
+  {KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_QUOT},
+  {_      , _      , X      , X      , X      , X      , X      , X      , X      , X      , X      , KC_RSFT},
+  {_      , _      , _      , _      , _      , _      , _      , _      , _      , _      , _      , _      }
 },
 
-/* Raise
- * ,-----------------------------------------------------------------------------------.
- * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
- * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO # |ISO / |      |      |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |Reset |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
- * `-----------------------------------------------------------------------------------'
- */
-[2] = {
-  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
-  {KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS},
-  {_______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, _______, _______, _______},
-  {RESET,   _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY}
+[ORNG] = {
+  {X      , X      , X      , X      , X      , X      , X      , KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS, KC_PIPE},
+  {X      , X      , X      , X      , X      , X      , X      , KC_LBRC, KC_RBRC, KC_MINS, KC_EQL,  KC_BSLS},
+  {_      , _      , X      , X      , X      , X      , X      , X      , X      , X      , X      , KC_RSFT},
+  {_      , _      , _      , _      , _      , _      , _      , _      , _      , _      , _      , _      }
 },
 
+[BLUE] = {
+  {MU_ON,   X      , KC_UP,   X      , X      , X      , X      , X      , KC_INS,  X      , KC_PSCR, KC_DEL },
+  {MU_OFF,  KC_LEFT, KC_DOWN, KC_RGHT, X      , X      , KC_HOME, KC_PGDN, KC_PGUP, KC_END,  X      , X      },
+  {_      , _      , KC_MUTE, KC_VOLD, KC_VOLU, X      , X      , X      , X      , X      , X      , KC_RSFT},
+  {RESET,   _      , _      , _      , _      , _      , _      , _      , _      , _      , _      , _      }
+},
 
 };
 
@@ -75,18 +66,13 @@ const uint16_t PROGMEM fn_actions[] = {
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   switch(id) {
-    case 1:
+    case PURP:
+    case ORNG:
+    case BLUE:
       if (record->event.pressed) {
-        layer_on(1);
+        layer_on(id);
       } else {
-        layer_off(1);
-      }
-      break;
-    case 2:
-      if (record->event.pressed) {
-        layer_on(2);
-      } else {
-        layer_off(2);
+        layer_off(id);
       }
       break;
   }
