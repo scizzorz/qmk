31f33344a2a30d80f184b2e89faa0810d82a8b85
John Weachock <jweachock@gmail.com>
2018-10-13 17:03:04 -0400

    Rearrange bottom row and function layer

--- keyboards/planck/keymaps/scizzorz/keymap.c
+++ keyboards/planck/keymaps/scizzorz/keymap.c
@@ -22,8 +22,8 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _FN,
   _NUM,
+  _FN,
 };
 
 enum planck_keycodes {
@@ -41,24 +41,24 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL, KC_LGUI, KC_LALT, KC_CAPS, NUM,     _______, _______, KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, FN
-),
-
-[_FN] = LAYOUT_planck_grid(
-    KC_GRV,  _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_DEL,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-    _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+  KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  KC_CAPS, _______, KC_LGUI, KC_LALT, NUM,     FN,      KC_LCTL, KC_SPC,  KC_RALT, KC_RGUI, _______, KC_RCTL
 ),
 
 [_NUM] = LAYOUT_planck_grid(
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_LPRN, KC_RPRN, KC_BSLS,
-    _______, KC_5,    KC_6,    KC_7,    KC_8,    KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_LBRC, KC_RBRC, _______,
-    _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_LCBR, KC_RCBR, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LABK, KC_RABK, _______
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, KC_LPRN, KC_RPRN, _______, _______, _______, KC_BSLS,
+  _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______,
+  _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_LCBR, KC_RCBR, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_FN] = LAYOUT_planck_grid(
+  _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_DEL,
+  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
+  _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
