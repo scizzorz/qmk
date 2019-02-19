e6109a31508dfe70317765a78b2d40bdc4434c37
John Weachock <weachockjr@corning.com>
2019-02-19 10:15:38 -0500

    Swap bottom row and add unused layer 3

--- keyboards/preonic/keymaps/scizzorz/keymap.c
+++ keyboards/preonic/keymaps/scizzorz/keymap.c
@@ -20,6 +20,7 @@ enum preonic_layers {
   _QWERTY,
   _NUM,
   _FN,
+  _LYR3,
 };
 
 enum preonic_keycodes {
@@ -33,6 +34,7 @@ enum preonic_keycodes {
 
 #define FN MO(_FN)
 #define NUM MO(_NUM)
+#define LYR3 MO(_LYR3)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -41,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_CAPS, _______, KC_LGUI, KC_LALT, NUM,     FN,      KC_LCTL, KC_SPC,  KC_RALT, KC_RGUI, _______, KC_RCTL  \
+  KC_LCTL, _______, KC_LGUI, KC_LALT, NUM,     FN,      LYR3,    KC_SPC,  KC_RALT, KC_RGUI, _______, KC_RCTL  \
 ),
 
 [_NUM] = LAYOUT_preonic_grid( \
@@ -58,6 +60,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
   _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______
+),
+
+[_LYR3] = LAYOUT_preonic_grid( \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
 
 /* Adjust (Lower + Raise)
