588e983ed0985732a8f4e6bbb77bf4b32ae9fc4d
John Weachock <weachockjr@corning.com>
2019-09-26 14:00:01 -0400

    Add number row to top and rename layers

--- keyboards/planck/keymaps/scizzorz/keymap.c
+++ keyboards/planck/keymaps/scizzorz/keymap.c
@@ -22,8 +22,8 @@ extern keymap_config_t keymap_config;
 
 enum planck_layers {
   _QWERTY,
-  _NUM,
-  _FN,
+  _RAISE,
+  _LOWER,
 };
 
 enum planck_keycodes {
@@ -35,8 +35,8 @@ enum planck_keycodes {
   EXT_PLV
 };
 
-#define FN MO(_FN)
-#define NUM MO(_NUM)
+#define LOWER MO(_LOWER)
+#define RAISE MO(_RAISE)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,18 +44,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL, KC_CAPS, KC_LGUI, KC_LALT, NUM,     FN,      KC_BSPC, KC_SPC,  KC_CAPS, KC_RGUI, KC_RALT, KC_RCTL  \
+  KC_LCTL, KC_CAPS, KC_LGUI, KC_LALT, RAISE,   LOWER,   KC_BSPC, KC_SPC,  KC_CAPS, KC_RGUI, KC_RALT, KC_RCTL  \
 ),
 
-[_NUM] = LAYOUT_planck_grid(
-  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______, _______, KC_BSLS,
+[_RAISE] = LAYOUT_planck_grid(
+  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
   _______, KC_5,    KC_6,    KC_7,    KC_8,    _______, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, _______, _______,
   _______, KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_DOT,  KC_LCBR, KC_RCBR, KC_LT,   KC_GT,   _______, _______,
   _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
 ),
 
-[_FN] = LAYOUT_planck_grid(
-  RESET,   _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, KC_DEL,
+[_LOWER] = LAYOUT_planck_grid(
+  _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, RESET,
   _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
   _______, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______
